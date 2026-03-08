e1d0a2d4cebe6c6d787b39bad58d7321b1ac7029
Patriks <p.kulinskis@gmail.com>
2026-03-08 14:21:12 +0200

    Renamed p1_points -> human_points, p2_points -> ai_points, added __init__.py so import works

--- src/game/node.py
+++ src/game/node.py
@@ -1,9 +1,9 @@
 # Klase priekš vienas virsotnes (stāvokļa) spēles kokā
 class Node:
-    def __init__(self, sequence, p1_points, p2_points, player_turn, parent=None, level=0, heuristic_val=None):
+    def __init__(self, sequence, human_points, ai_points, player_turn, parent=None, level=0, heuristic_val=None):
         self.sequence = sequence        # list of 0s and 1s
-        self.p1_points = p1_points      # human points
-        self.p2_points = p2_points      # Ai points
+        self.human_points = human_points      # human points
+        self.ai_points = ai_points      # Ai points
         self.player_turn = player_turn  # 0 = human, 1 = Ai
         self.parent = parent            # vecāka virsotne vai None, ja tā ir sakne
         self.level = level              # līmenis kokā
